a594374f8eac81f19cee1b12a4c7b180126fabe0
omg.dobcom <omg.dobcom@gmail.com>
2022-11-22 15:59:36 +0300

    homework3 done

---
+++ task19/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача №19. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.WriteLine("Введите пятизначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int num1 = number / 10000 % 10;
+int num2 = number / 1000 % 10;
+int num4 = number / 10 % 10;
+int num5 = number % 10;
+
+Console.Write("Является ли полиндоромом: ");
+
+if (num1 == num5 && num2 == num4)
+{
+    Console.WriteLine($"{number} => да");
+}
+else
+{
+    Console.WriteLine($"{number} => нет");
+}
---
+++ task23/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Введите число");
+var num = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1;i <= num;i++)
+{
+    Console.WriteLine(Math.Pow(i,3));
+}
